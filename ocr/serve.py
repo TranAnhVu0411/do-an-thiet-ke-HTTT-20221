2b556da0cfe23407bb1131bf6c99a78dffdfe601
hiennguyennq <93762976+hiennguyennq@users.noreply.github.com>
2023-02-06 01:53:25 +0700

    Add files via upload

---
+++ ocr/serve.py
@@ -0,0 +1,125 @@
+import numpy as np
+from flask import Flask, request
+import json
+import os
+import easyocr
+from transformer_model import TransformerModel
+from ultis import no_accent_vietnamese
+from pdf2image import convert_from_path
+from PIL import Image
+import base64
+
+app = Flask(__name__)
+
+def resize(quality, image):
+	if quality == "low":
+		new_image = image.resize((400, 600))
+	if quality == "medium":
+		new_image = image.resize((800, 1200))
+	if quality == "high":
+		new_image = image
+	return new_image
+
+@app.route("/image_to_text", methods=["POST"])
+def _image_to_text():
+	data = {"success": False}
+	if request.method == "POST":
+		#convert base64 to image
+		encoded_string = request.form.get('image', '')
+		with open("image.jpg", "wb") as image_file:
+			image_file.write(base64.b64decode(encoded_string))
+		image = Image.open("image.jpg")
+		
+		#select quality image
+		quality = request.form.get('quality', '')
+		new_image = resize(quality, image)
+		new_image.save('new_image' +'.jpg')
+		result = reader.readtext('new_image.jpg', paragraph="False")
+		text = ''
+		for i in result:
+			text = text + i[1]
+		# text_ = text.split('.')
+		# text_1 = ''
+		# for i in text_:
+		# 	text_1 = text + transformer.predict(no_accent_vietnamese(i))	
+		# data["text_1"] = text_1
+		data["text"] = text
+		data["success"] = True
+		os.remove("image.jpg")
+		os.remove("new_image.jpg")
+	return json.dumps(data, ensure_ascii=False)
+
+@app.route("/images_to_text", methods=["POST"])
+def _images_to_text():
+	data = {"success": False}
+	if request.method == "POST":
+		images = request.form.get('image', '').split(',')
+		for image in images:
+			image = Image.open(image)
+			new_image = image.resize((1200, 1600))
+			new_image.save('new_image' +'.jpg')
+			result = reader.readtext('new_image.jpg', paragraph="False")
+			text = ''
+			for i in result:
+				text = text + i[1]
+			# text_ = text.split('.')
+			# text_1 = ''
+			# for i in text_:
+			# 	text_1 = text + transformer.predict(no_accent_vietnamese(i))	
+			# data["text_1"] = text_1
+		data["text"] = text
+		data["success"] = True
+	return json.dumps(data, ensure_ascii=False)
+
+@app.route("/link_image_to_text", methods=["POST"])
+def _link_image_to_text():
+	data = {"success": False}
+	if request.method == "POST":
+		image = request.form['link']
+		result = reader.readtext(image, paragraph="False")
+		text = ''
+		for i in result:
+			text = text + i[1]
+		# text_ = text.split('.')
+		# text_1 = ''
+		# for i in text_:
+		# 	text_1 = text + transformer.predict(no_accent_vietnamese(i))	
+		# data["text_1"] = text_1
+		data["text"] = text
+		data["success"] = True
+	return json.dumps(data, ensure_ascii=False)
+
+@app.route("/pdf_to_text", methods=["POST"])
+def _pdf_to_text():
+	data = {"success": False}
+	if request.method == "POST":
+		text = ''
+		pdf = request.form.get('pdf', '')
+		images = convert_from_path(pdf, 500, poppler_path=r"poppler-22.12.0\Library\bin")
+ 
+		for i in range(len(images)):
+			images[i].save('page'+ str(i) +'.jpg', 'JPEG')
+			image = Image.open('page'+ str(i) +'.jpg')
+			image = image.resize((800, 1100))
+			image.save('page'+ str(i) +'.jpg')
+			result = reader.readtext('page'+ str(i) +'.jpg', paragraph="False")
+			for j in result:
+				text = text + j[1]
+			del(result)
+
+		data["text"] = text
+		data["success"] = True
+	return json.dumps(data, ensure_ascii=False)
+
+@app.route("/test", methods=["POST"])
+def _test():
+	if request.method == "POST":
+		return request.form.get('link', '')
+
+if __name__ == "__main__":
+	reader = easyocr.Reader(['vi'])
+	# transformer = TransformerModel(source_vectorization='source_vectorization_layer.pkl',
+	# target_vectorization='target_vectorization_layer.pkl',
+	# model_path='restore_diacritic.keras')
+	print("App run!")
+	app.run(debug=True)
---
+++ ocr/ultis.py
@@ -0,0 +1,42 @@
+import re
+import easyocr
+import requests
+def no_accent_vietnamese(s):
+    s = re.sub(r'[àáạảãâầấậẩẫăằắặẳẵ]', 'a', s)
+    s = re.sub(r'[ÀÁẠẢÃĂẰẮẶẲẴÂẦẤẬẨẪ]', 'A', s)
+    s = re.sub(r'[èéẹẻẽêềếệểễ]', 'e', s)
+    s = re.sub(r'[ÈÉẸẺẼÊỀẾỆỂỄ]', 'E', s)
+    s = re.sub(r'[òóọỏõôồốộổỗơờớợởỡ]', 'o', s)
+    s = re.sub(r'[ÒÓỌỎÕÔỒỐỘỔỖƠỜỚỢỞỠ]', 'O', s)
+    s = re.sub(r'[ìíịỉĩ]', 'i', s)
+    s = re.sub(r'[ÌÍỊỈĨ]', 'I', s)
+    s = re.sub(r'[ùúụủũưừứựửữ]', 'u', s)
+    s = re.sub(r'[ƯỪỨỰỬỮÙÚỤỦŨ]', 'U', s)
+    s = re.sub(r'[ỳýỵỷỹ]', 'y', s)
+    s = re.sub(r'[ỲÝỴỶỸ]', 'Y', s)
+    s = re.sub(r'[Đ]', 'D', s)
+    s = re.sub(r'[đ]', 'd', s)
+    s = re.sub(r'["]', '', s)
+    s = re.sub(r"[']", '', s)
+    return s
+pic_url = 'https://drive.google.com/file/d/1a-6X5dmFtkRcBny6PtWx4JjUN1bOLBCu/view?usp=share_link'
+import requests
+with open('FB_IMG_1490534565948.jpg', 'wb') as f:
+    f.write(requests.get(pic_url).content)
+
+
+# img_data = requests.get('https://github.com/hiennguyennq/Toxic-comment-classification/blob/main/Untitled03.png').content
+# with open('image_name.jpg', 'wb') as handler:
+#     handler.write(img_data)
+# reader = easyocr.Reader(['vi'])
+# result = reader.readtext(img_data, paragraph="False")
+# print(result)
+
+from pdf2image import convert_from_path
+ 
+from PIL import Image
+
+# Store Pdf with convert_from_path function
+image = Image.open('page'+ str(0) +'.jpg')
+image = image.resize((600, 800))
+image.save('page'+ str('x') +'.jpg')
